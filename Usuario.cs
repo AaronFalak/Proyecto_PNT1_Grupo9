1b032b69c43ed58eacbb796cb429b7d2d4287aa8
AaronFalak <aaronfalak@gmail.com>
2024-10-13 21:53:11 -0300

    2024-10-13_ClaseUsuario

---
+++ Usuario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Usuario {
+
+    public string id;
+	public string nombre;
+	public string email;
+	public DateTime fechaAlta;
+	public string password;
+	public string apellido;
+	public string dni;
+	public string telefono;
+
+
+
+    public Usuario() {
+	}
+}
